cf0b97d9951d85aeb5adf4cb62b200cabf45d008
Máté Gál <gal.mateo@gmail.com>
2017-02-27 19:54:39 +0100

    Update main_DC.py

--- client/main_DC.py
+++ client/main_DC.py
@@ -4,18 +4,23 @@ import wx
 class MainFrame(wx.Frame):
 
 	def __init__(self):
-		wx.Frame.__init__(self, None, title='Test')
+		wx.Frame.__init__(self, None, title='FIPER')
+		resolution = wx.DisplaySize()
+		self.width = resolution[0] 
+		self.height = resolution[1]
+		
+		self.Bind(wx.EVT_KEY_UP, self.on_key_down)
+		
 		# Set App to FullScreen and get focus
-		self.ShowFullScreen(1)
+		self.SetBackgroundColour('black')
+		self.ShowFullScreen(True)
+		# wx.Sleep(1)
 		# Erase background from designated areas by DC ( for the buttons )
 		self.Bind(wx.EVT_ERASE_BACKGROUND, self.set_background)
 		# Set Custom Cursor Image 
 		self.set_cursor()
 		# Place buttons on background
 		self.place_buttons()
-		
-		self.place_labels()
-		
 		# Show Application ( Grab Focus )
 		self.Show()
 		
@@ -46,41 +51,80 @@ class MainFrame(wx.Frame):
 		cursor = wx.Cursor(cursor_path, wx.BITMAP_TYPE_ICO, 6, 28)
 		self.SetCursor(cursor)	
 		
+	def on_key_down(self, event):
+		if (event.GetKeyCode() == 27): # Check if ESC is pressed
+			self.close_app(wx.EVT_BUTTON)
+		else:
+			event.Skip()	
+		
 	def place_buttons(self):
 		resolution = wx.DisplaySize()
 		width = resolution[0] 
 		height = resolution[1]
 		
-		connect_skin = wx.Bitmap('image\\menu\\buttons\\btn0.png')
-		connect_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn0_hover.png')
-		connect_skin_click = wx.Bitmap('image\\menu\\buttons\\btn0_click.png')
-		connect_button = wx.BitmapButton(self, -1, connect_skin, pos=(int(width*0.65),int(height*0.3)), size=(510,130))
-		connect_button.SetBitmapHover(connect_skin_hover)
-		connect_button.SetBitmapSelected(connect_skin_click)
+		self.connect_skin = wx.Bitmap('image\\menu\\buttons\\btn0.png')
+		self.connect_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn0_hover.png')
+		self.connect_skin_click = wx.Bitmap('image\\menu\\buttons\\btn0_click.png')
+		self.connect_button = wx.BitmapButton(self, -1, self.connect_skin, pos=(int(width*0.65),int(height*0.3)), size=(510,130))
+		self.connect_button.SetBitmapHover(self.connect_skin_hover)
+		self.connect_button.SetBitmapSelected(self.connect_skin_click)
 	
-		options_skin = wx.Bitmap('image\\menu\\buttons\\btn1.png')
-		options_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn1_hover.png')
-		options_skin_click = wx.Bitmap('image\\menu\\buttons\\btn1_click.png')
-		options_button = wx.BitmapButton(self, -1, options_skin, pos=(int(width*0.65),int(height*0.5)), size=(510,130))
-		options_button.SetBitmapHover(options_skin_hover)
-		options_button.SetBitmapSelected(options_skin_click)
+		self.options_skin = wx.Bitmap('image\\menu\\buttons\\btn1.png')
+		self.options_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn1_hover.png')
+		self.options_skin_click = wx.Bitmap('image\\menu\\buttons\\btn1_click.png')
+		self.options_button = wx.BitmapButton(self, -1, self.options_skin, pos=(int(width*0.65),int(height*0.5)), size=(510,130))
+		self.options_button.SetBitmapHover(self.options_skin_hover)
+		self.options_button.SetBitmapSelected(self.options_skin_click)
+		self.options_button.Bind(wx.EVT_BUTTON, self.options_window)
 		
-		quit_skin = wx.Bitmap('image\\menu\\buttons\\btn2.png')
-		quit_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn2_hover.png')
-		quit_skin_click = wx.Bitmap('image\\menu\\buttons\\btn2_click.png')
-		quit_button = wx.BitmapButton(self, -1, quit_skin, pos=(int(width*0.65),int(height*0.7)), size=(510,130))
-		quit_button.SetBitmapHover(quit_skin_hover)
-		quit_button.SetBitmapSelected(quit_skin_click)
-		self.Bind(wx.EVT_BUTTON, self.close_app, quit_button)
-		
-	def place_labels(self):
-		resolution = wx.DisplaySize()
-		width = resolution[0] 
-		height = resolution[1]
+		self.quit_skin = wx.Bitmap('image\\menu\\buttons\\btn2.png')
+		self.quit_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn2_hover.png')
+		self.quit_skin_click = wx.Bitmap('image\\menu\\buttons\\btn2_click.png')
+		self.quit_button = wx.BitmapButton(self, -1, self.quit_skin, pos=(int(width*0.65),int(height*0.7)), size=(510,130))
+		self.quit_button.SetBitmapHover(self.quit_skin_hover)
+		self.quit_button.SetBitmapSelected(self.quit_skin_click)
+		self.quit_button.Bind(wx.EVT_BUTTON, self.close_app)
+
+	def options_window(self, event):
+		x_pos = (self.width * 0.1)
+		y_pos = (self.height * 0.1)
+		width = (self.width * 0.7)
+		height = (self.height * 0.3)
+		self.options_window = NewFrame('options',width,height,x_pos,y_pos)
 		
 	def close_app(self, event):
-		self.Close(True)
+		self.quit_window = NewFrame('quit',200,200,200,200)
 		
+		
+		
+class NewFrame(wx.Frame):
+	
+	def __init__(self, title, width, height, x, y):
+		style = ( wx.CLIP_CHILDREN | wx.STAY_ON_TOP | wx.FRAME_NO_TASKBAR |
+                  wx.NO_BORDER | wx.FRAME_SHAPED  )
+		wx.Frame.__init__(self, None, title=title, style = style)
+		
+		self.SetPosition((x,y))
+		self.Size.Set(width,height)
+		
+		self.SetTransparent(120)
+		self.SetBackgroundColour('Black')
+		self.Bind(wx.EVT_KEY_UP, self.on_key_down)
+		
+		self.set_cursor()
+		self.Show()
+
+	def set_cursor(self):
+		cursor_path = ('image\\menu\\cursor.ico')
+		cursor = wx.Cursor(cursor_path, wx.BITMAP_TYPE_ICO, 6, 28)
+		self.SetCursor(cursor)
+	
+	def on_key_down(self, event):
+		if (event.GetKeyCode() == 27): # Check if ESC is pressed
+			self.Close(force=True)
+		else:
+			event.Skip()
+			
 class Main(wx.App):
    
     def __init__(self):
