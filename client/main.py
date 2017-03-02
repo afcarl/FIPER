8edf4856cbe16d4ffe46a868a39a3b4db07ba022
Máté Gál <gal.mateo@gmail.com>
2017-03-02 17:11:00 +0100

    Update main.py

--- client/main.py
+++ client/main.py
@@ -2,8 +2,8 @@ import wx
 
 class MainFrame(wx.Frame):
 
-	def __init__(self):
-		wx.Frame.__init__(self, None, title='FIPER')
+	def __init__(self, place_buttons):
+		wx.Frame.__init__(self, None, place_buttons, title='FIPER')
 		# Get screen parameters
 		resolution = wx.DisplaySize()
 		self.width = resolution[0] 
@@ -14,14 +14,16 @@ class MainFrame(wx.Frame):
 		self.SetBackgroundColour('black')
 		# Erase background from designated areas by DC ( for the buttons )
 		self.Bind(wx.EVT_ERASE_BACKGROUND, self.set_background)
+		# Place buttons on background
+		if ( place_buttons == True ):
+			self.place_buttons() 
 		# Set Custom Cursor Image 
 		self.set_cursor()
-		# Place buttons on background
-		self.place_buttons()
 		# Show Application ( Grab Focus )
 		self.Show()
 		
 	def set_background(self, event): 
+		# Setting up Background with Transparency and ClippingRegion
 		self.SetBackgroundStyle(wx.BG_STYLE_CUSTOM)
 		dc = event.GetDC()
 		if not dc:
@@ -29,30 +31,30 @@ class MainFrame(wx.Frame):
 			rect = self.GetUpdateRegion().GetBox()
 			dc.SetClippingRegionAsRegion()
 		dc.Clear()
-		
+		# Image sources
 		background_image = wx.Image('image\\menu\\bgnd.jpg').ConvertToBitmap()
 		fiper_logo = wx.Image('image\\menu\\fiper_logo.png').ConvertToBitmap()
-		
-		background_bitmap = self.scale_bitmap(background_image, self.width, self.height)
-		dc.DrawBitmap(background_bitmap, 0, 0, True)
-		fiper_logo = self.scale_bitmap(fiper_logo, self.width*0.4, self.height*0.3)
-		dc.DrawBitmap(fiper_logo, 40, -10, False)
+		# Scale and set textures
+		self.background_bitmap = self.scale_bitmap(background_image, self.width, self.height)
+		dc.DrawBitmap(self.background_bitmap, 0, 0, True)
+		self.fiper_logo = self.scale_bitmap(fiper_logo, self.width*0.4, self.height*0.3)
+		dc.DrawBitmap(self.fiper_logo, 40, -10, False)
 		
 	def scale_bitmap(self, bitmap, width, height):
+		# This method scales a bitmap for a desired size
 		image = wx.ImageFromBitmap(bitmap)
 		image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
 		result = wx.BitmapFromImage(image)
 		return result	
 		
 	def set_cursor(self):
+		# Custom cursor for the application
 		cursor_path = ('image\\menu\\cursor.ico')
 		cursor = wx.Cursor(cursor_path, wx.BITMAP_TYPE_ICO, 6, 28)
 		self.SetCursor(cursor)	
 		
-	def place_labels(self):
-		self.label_email = label.transparent_text(self, 'hello', (200,200), 'green', 32)
-		
 	def place_buttons(self):
+		# Place buttons on the background and define the behaviour of them 
 		self.connect_skin = wx.Bitmap('image\\menu\\buttons\\btn0.png')
 		self.connect_skin_hover = wx.Bitmap('image\\menu\\buttons\\btn0_hover.png')
 		self.connect_skin_click = wx.Bitmap('image\\menu\\buttons\\btn0_click.png')
@@ -84,21 +86,23 @@ class MainFrame(wx.Frame):
 													size=(510,130))
 		self.quit_button.SetBitmapHover(self.quit_skin_hover)
 		self.quit_button.SetBitmapSelected(self.quit_skin_click)
-		self.quit_button.Bind(wx.EVT_BUTTON, self.quit_window)
+		self.quit_button.Bind(wx.EVT_BUTTON, self.quit_window)	
+		
 		
 	def connect_window(self, event):
 		x_pos = (self.width * 0.025)
 		y_pos = (self.height * 0.025)
 		width = (self.width * 0.95)
 		height = (self.height * 0.95)
-		self.options_window = NewFrame('connect',width,height,x_pos,y_pos)
+		self.connect_window = NewFrame('connect', width, height, x_pos, y_pos)
+
 		
 	def options_window(self, event):
 		x_pos = (self.width * 0.025)
 		y_pos = (self.height * 0.025)
 		width = (self.width * 0.95)
 		height = (self.height * 0.95)
-		self.options_window = NewFrame('options',width,height,x_pos,y_pos)
+		self.options_window = NewFrame('options', width, height, x_pos, y_pos)
 		
 	def quit_window(self, event):
 		x_pos = (self.width * 0.35)
@@ -107,22 +111,36 @@ class MainFrame(wx.Frame):
 		height = (self.height * 0.3)
 		self.quit_window = NewFrame('quit', width, height, x_pos, y_pos)
 		
+	def hide_buttons(self):
+		self.background_bitmap.Destroy()
+		self.fiper_logo.Destroy()
+		self.__init__(False)
+		
+		self.connect_button.Hide()
+		self.options_button.Hide()
+		self.quit_button.Hide()
+
 		
 class NewFrame(wx.Frame):
 	
 	def __init__(self, title, width, height, x, y):
-		style = ( wx.CLIP_CHILDREN | wx.STAY_ON_TOP | wx.FRAME_NO_TASKBAR |
-                  wx.NO_BORDER | wx.FRAME_SHAPED  )
+		style = ( 	wx.CLIP_CHILDREN | wx.STAY_ON_TOP | 
+					wx.FRAME_NO_TASKBAR | wx.NO_BORDER | wx.FRAME_SHAPED  )
 		wx.Frame.__init__(self, None, title=title, style = style)
 		# Set Position and Size of new frame
 		self.SetPosition((x,y))
 		self.SetSize((width,height))
-		# Make new frame transparent, black,
-		self.SetTransparent(200)
-		self.SetBackgroundColour('Black')
-		# Keep Cursor Shape for new Frame 
-		self.set_cursor()
-		self.Show()
+		# Make new frame transparent, black, and fade in
+		tp = 0
+		while(1):
+			self.SetTransparent(tp)
+			wx.Sleep(.001)
+			tp += 10 
+			self.SetBackgroundColour('black')
+			# Keep Cursor Shape for new Frame 
+			self.set_cursor()
+			self.Show()
+			if ( tp == 150 ): break # Max transparency
 
 	def set_cursor(self):
 		cursor_path = ('image\\menu\\cursor.ico')
@@ -130,12 +148,11 @@ class NewFrame(wx.Frame):
 		self.SetCursor(cursor)
 			
 		
-	
 class Main(wx.App):
    
     def __init__(self):
 		wx.App.__init__(self)
-		dlg = MainFrame()
+		dlg = MainFrame(True)
 		dlg.Show()
 
 if __name__ == "__main__":
