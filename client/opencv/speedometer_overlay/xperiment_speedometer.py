20b7b477b04a3bfe7707a234517965fb6fb753b7
Csxeba <csxeba@gmail.com>
2017-08-27 19:55:40 +0200

    Added comments

--- client/opencv/speedometer_overlay/xperiment_speedometer.py
+++ client/opencv/speedometer_overlay/xperiment_speedometer.py
@@ -3,15 +3,21 @@
 import cv2
 import numpy as np
 
+# Speedometer pointer
 speedin = cv2.imread("speedin.jpg")  # type: np.ndarray
+# Speedometer outer part
 speedout = cv2.imread("speedout.jpg")  # type: np.ndarray
 
+# Shapes
 ishx, ishy, ishz = speedin.shape
 oshx, oshy, oshz = speedout.shape
+
+# Appropriate translation for the pointer part
 trX, trY = (oshx - ishx) // 2, (oshy - ishy) // 2
 
 
 def kmph_stream():
+    """Generates random velocity values"""
     velocity = 0.
     while 1:
         velocity *= 0.95
@@ -20,6 +26,7 @@ def kmph_stream():
 
 
 def cam_stream():
+    """Reads the webcam for video frames"""
     # noinspection PyArgumentList
     dev = cv2.VideoCapture(0)
     while 1:
@@ -31,35 +38,46 @@ def cam_stream():
 
 
 def get_speedometer(kmph):
+    """Constructs a speedometer with the pointer set to the appropriate kmph value"""
     if kmph >= 240:
         kmph = 240
     deg = 30 - kmph
-    rotM = cv2.getRotationMatrix2D((ishx//2, ishy//2), deg, 1)
-    rotIn = cv2.warpAffine(speedin, rotM, (ishx, ishy))
+    rotM = cv2.getRotationMatrix2D((ishx // 2, ishy // 2), deg, 1)
+    rotIn = cv2.warpAffine(speedin, rotM, (ishx, ishy))  # Rotated pointer
     mat = speedout.copy()
-    mat[trX:trX+ishx, trY:trY+ishy] = rotIn
+    # Replace speedout's appropriate part with the rotated speedin image
+    mat[trX:trX + ishx, trY:trY + ishy] = rotIn
     return mat
 
 
 def decorate_frame(frame, kmph):
+    """Decorates a video frame with a speedometer and a text message"""
     speedometer = get_speedometer(kmph)
     output = frame.copy()
+    # Addition below ensures that [0, 0, 0] pixels are transparent
     output[-oshx:, :oshy] += speedometer
-    cv2.putText(output, "{} km/h".format(kmph), (20, 20), 1, 1, (255, 255, 255))
-    return np.clip(output, 0, 255)
+    text_bottom_left = (20, 20)
+    font_face = 1
+    font_scale = 1
+    font_color = (255, 255, 255)
+    cv2.putText(output, "{} km/h".format(kmph), text_bottom_left,
+                font_face, font_scale, font_color)
+    return np.clip(output, 0, 255)  # to stay in valid pixel value range
 
 
 def main():
     speeds = kmph_stream()
     camframes = cam_stream()
-    frame = next(camframes)
-    fourcc = cv2.VideoWriter_fourcc(*"DIVX")
-    vw = cv2.VideoWriter("speedometer.avi", fourcc, 20.0, frame.shape[:2][::-1])  # type: cv2.VideoWriter
+    frame = next(camframes)  # Get a single frame for shape info
+    fourcc = cv2.VideoWriter_fourcc(*"DIVX")  # Select codec
+    vidshape = frame.shape[:2][::-1]  # Drop 3rd dimension and reverse x - y
+    vw = cv2.VideoWriter("speedometer.avi", fourcc, 20.0, vidshape)
     for i in range(300):
         print "\r", i,
         deco = decorate_frame(next(camframes), next(speeds))
         vw.write(deco)
     vw.release()
+
 
 if __name__ == '__main__':
     main()
